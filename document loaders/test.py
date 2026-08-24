a0768a9310d6a2ce49e0f2a4b3d8d3713261f3ed
Aatmaj Patro <aatmajpatro@gmail.com>
2026-08-25 00:26:52 +0530

    Refactor document loading and prompt creation in main.py; update test.py for correct file path handling

--- document loaders/test.py
+++ document loaders/test.py
@@ -1,3 +1,18 @@
+#this is the orginal code but due to error not able to run the code 
+# so i have changed the code to this one and it is working fine
+#from langchain_community.document_loaders import TextLoader
+#data = TextLoader("example.txt").load()
+#print(data)
+
+
+from pathlib import Path
+
 from langchain_community.document_loaders import TextLoader
-data = TextLoader("document loaders/example.txt").load()
-print(data)
+
+data = TextLoader(
+	str(Path(__file__).parent / "example.txt"),
+	encoding="utf-8",
+).load()
+print(data[0].page_content  )
+
+
--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 from dotenv import load_dotenv
 load_dotenv()  # Load environment variables from .env file
-
 from langchain_mistralai import ChatMistralAI
+from langchain_community.document_loaders import TextLoader
+from langchain_core.prompts import ChatPromptTemplate
+from pathlib import Path
 
+# Load the document
+data = TextLoader(
+    str(Path(__file__).parent / "document loaders" / "example.txt"),
+    encoding="utf-8",
+).load()
+
+# Create a prompt template for assigining role to the model
+template=ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a ai that summarizes text."),
+        ("user", "{data}"),
+    ]
+)   
+
+# Invoke the model with the prompt
 model = ChatMistralAI(model='mistral-small-2603')
-res=model.invoke("What is the capital of France?")
+prompt = template.format_prompt(data=data[0].page_content)
+res=model.invoke(prompt)
 print(res.content)
